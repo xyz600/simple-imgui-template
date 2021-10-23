a4be746ae9de30717aeae6952bdeb3ac4c8f1c3f
xyz600 <xyz600600@gmail.com>
2021-10-23 12:27:32 +0900

    remove implot(linux is not supported)

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,14 @@
 // Dear ImGui: standalone example application for GLFW + OpenGL2, using legacy fixed pipeline
-// (GLFW is a cross-platform general purpose library for handling windows, inputs, OpenGL/Vulkan/Metal graphics context creation, etc.)
-// If you are new to Dear ImGui, read documentation from the docs/ folder + read the top of imgui.cpp.
+// (GLFW is a cross-platform general purpose library for handling windows, inputs, OpenGL/Vulkan/Metal graphics context
+// creation, etc.) If you are new to Dear ImGui, read documentation from the docs/ folder + read the top of imgui.cpp.
 // Read online: https://github.com/ocornut/imgui/tree/master/docs
 
+#include <GLFW/glfw3.h>
+#include <iostream>
+#include <memory>
 #include "imgui.h"
 #include "imgui_impl_glfw.h"
 #include "imgui_impl_opengl2.h"
-#include <GLFW/glfw3.h>
-#include <memory>
-#include <iostream>
-#include "implot.h"
 
 static void glfw_error_callback(int error, const char* description)
 {
@@ -20,30 +19,32 @@ class ImguiOpenGL2Viewer
 {
 public:
     inline ImguiOpenGL2Viewer(const std::string title, const int height, const int width)
-        : m_window_(nullptr), m_show_demo_window_(false), m_show_another_window_(true), m_clear_color_(0.45f, 0.55f, 0.60f, 1.00f)
+        : m_window_(nullptr),
+          m_show_demo_window_(false),
+          m_show_another_window_(true),
+          m_clear_color_(0.45f, 0.55f, 0.60f, 1.00f)
     {
         // Setup window
         glfwSetErrorCallback(glfw_error_callback);
-        if (!glfwInit())
-            return;
+        if (!glfwInit()) return;
 
         m_window_ = glfwCreateWindow(width, height, title.c_str(), nullptr, nullptr);
-        if (m_window_ == nullptr)
-            return;
+        if (m_window_ == nullptr) return;
 
         glfwMakeContextCurrent(m_window_);
-        glfwSwapInterval(1); // Enable vsync
+        glfwSwapInterval(1);  // Enable vsync
 
         // Setup Dear ImGui context
         IMGUI_CHECKVERSION();
         ImGui::CreateContext();
-        ImGuiIO& io = ImGui::GetIO(); (void)io;
-        //io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
-        //io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+        ImGuiIO& io = ImGui::GetIO();
+        (void)io;
+        // io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+        // io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
 
         // Setup Dear ImGui style
         ImGui::StyleColorsDark();
-        //ImGui::StyleColorsClassic();
+        // ImGui::StyleColorsClassic();
 
         // Setup Platform/Renderer backends
         ImGui_ImplGlfw_InitForOpenGL(m_window_, true);
@@ -56,10 +57,12 @@ public:
         while (!glfwWindowShouldClose(m_window_))
         {
             // Poll and handle events (inputs, window resize, etc.)
-            // You can read the io.WantCaptureMouse, io.WantCaptureKeyboard flags to tell if dear imgui wants to use your inputs.
+            // You can read the io.WantCaptureMouse, io.WantCaptureKeyboard flags to tell if dear imgui wants to use
+            // your inputs.
             // - When io.WantCaptureMouse is true, do not dispatch mouse input data to your main application.
             // - When io.WantCaptureKeyboard is true, do not dispatch keyboard input data to your main application.
-            // Generally you may always pass all inputs to dear imgui, and hide them from your application based on those two flags.
+            // Generally you may always pass all inputs to dear imgui, and hide them from your application based on
+            // those two flags.
             glfwPollEvents();
 
             // Start the Dear ImGui frame
@@ -74,17 +77,18 @@ public:
             int display_w, display_h;
             glfwGetFramebufferSize(m_window_, &display_w, &display_h);
             glViewport(0, 0, display_w, display_h);
-            glClearColor(m_clear_color_.x * m_clear_color_.w, m_clear_color_.y * m_clear_color_.w, 
-                m_clear_color_.z * m_clear_color_.w, m_clear_color_.w);
+            glClearColor(m_clear_color_.x * m_clear_color_.w, m_clear_color_.y * m_clear_color_.w,
+                         m_clear_color_.z * m_clear_color_.w, m_clear_color_.w);
             glClear(GL_COLOR_BUFFER_BIT);
 
-            // If you are using this code with non-legacy OpenGL header/contexts (which you should not, prefer using imgui_impl_opengl3.cpp!!),
-            // you may need to backup/reset/restore other state, e.g. for current shader using the commented lines below.
-            //GLint last_program;
-            //glGetIntegerv(GL_CURRENT_PROGRAM, &last_program);
-            //glUseProgram(0);
+            // If you are using this code with non-legacy OpenGL header/contexts (which you should not, prefer using
+            // imgui_impl_opengl3.cpp!!), you may need to backup/reset/restore other state, e.g. for current shader
+            // using the commented lines below.
+            // GLint last_program;
+            // glGetIntegerv(GL_CURRENT_PROGRAM, &last_program);
+            // glUseProgram(0);
             ImGui_ImplOpenGL2_RenderDrawData(ImGui::GetDrawData());
-            //glUseProgram(last_program);
+            // glUseProgram(last_program);
 
             glfwMakeContextCurrent(m_window_);
             glfwSwapBuffers(m_window_);
@@ -92,72 +96,49 @@ public:
         close();
     }
 
-
 private:
-
     void update()
     {
         // 基本的にユーザがいじる関数
 
-        // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
-        if (m_show_demo_window_)
-            ImGui::ShowDemoWindow(&m_show_demo_window_);
+        // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code
+        // to learn more about Dear ImGui!).
+        if (m_show_demo_window_) ImGui::ShowDemoWindow(&m_show_demo_window_);
 
         // 2. Show a simple window that we create ourselves. We use a Begin/End pair to created a named window.
         {
             static float f = 0.0f;
             static int counter = 0;
 
-            ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
+            ImGui::Begin("Hello, world!");  // Create a window called "Hello, world!" and append into it.
 
-            ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
-            ImGui::Checkbox("Demo Window", &m_show_demo_window_);      // Edit bools storing our window open/close state
+            ImGui::Text("This is some useful text.");  // Display some text (you can use a format strings too)
+            ImGui::Checkbox("Demo Window", &m_show_demo_window_);  // Edit bools storing our window open/close state
             ImGui::Checkbox("Another Window", &m_show_another_window_);
 
-            ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+            ImGui::SliderFloat("float", &f, 0.0f, 1.0f);  // Edit 1 float using a slider from 0.0f to 1.0f
 
-            if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
+            if (ImGui::Button(
+                    "Button"))  // Buttons return true when clicked (most widgets return true when edited/activated)
                 counter++;
             ImGui::SameLine();
             ImGui::Text("counter = %d", counter);
 
-            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate,
+                        ImGui::GetIO().Framerate);
             ImGui::End();
         }
 
         // 3. Show another simple window.
         if (m_show_another_window_)
         {
-            ImGui::Begin("Another Window", &m_show_another_window_);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
+            ImGui::Begin("Another Window",
+                         &m_show_another_window_);  // Pass a pointer to our bool variable (the window will have a
+                                                    // closing button that will clear the bool when clicked)
             ImGui::Text("Hello from another window!");
-            if (ImGui::Button("Close Me"))
-                m_show_another_window_ = false;
+            if (ImGui::Button("Close Me")) m_show_another_window_ = false;
             ImGui::End();
         }
-
-        // 4. imgui plot
-        // TODO: fail to build @ wsl2
-        ImGui::Begin("implot");
-        ImGui::Separator();
-        ImGui::Checkbox("Anti-Aliased Lines", &ImPlot::GetStyle().AntiAliasedLines);
-        ImGui::Checkbox("Use Local Time", &ImPlot::GetStyle().UseLocalTime);
-        ImGui::Checkbox("Use ISO 8601", &ImPlot::GetStyle().UseISO8601);
-        ImGui::Checkbox("Use 24 Hour Clock", &ImPlot::GetStyle().Use24HourClock);
-        ImGui::Separator();
-        if (ImPlot::BeginPlot("Preview")) {
-            static double now = static_cast<double>(time(0));
-            // ImPlot::SetupAxis(ImAxis_X1,NULL,ImPlotAxisFlags_Time);
-            // ImPlot::SetupAxisLimits(ImAxis_X1, now, now + 24*3600);
-            for (int i = 0; i < 10; ++i) {
-                double x[2] = {now, now + 24*3600};
-                double y[2] = {0,i/9.0};
-                ImGui::PushID(i);
-                ImPlot::PlotLine("##Line",x,y,2);
-                ImGui::PopID();
-            }
-            ImPlot::EndPlot();
-        }
-        ImGui::End();
     }
 
     void close()
@@ -171,14 +152,13 @@ private:
         glfwTerminate();
     }
 
-    GLFWwindow *m_window_;
+    GLFWwindow* m_window_;
 
     bool m_show_demo_window_;
 
     bool m_show_another_window_;
 
     ImVec4 m_clear_color_;
-
 };
 
 int main(int, char**)
